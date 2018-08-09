8d924f0946e2fef46acdc141396a755878758857
Aaditya Menon <aaditya@sparker0i.me>
2018-08-10 02:39:40 +0530

    Builder Pattern in Java

---
+++ Builder Pattern/Java/ChickenBurger.java
@@ -0,0 +1,9 @@
+public class ChickenBurger extends Burger {
+    public float price() {
+        return 35.0f;
+    }
+
+    public String name() {
+        return "Chicken Burger";
+    }
+}
---
+++ Builder Pattern/Java/Pepsi.java
@@ -0,0 +1,9 @@
+public class Pepsi extends ColdDrink {
+    public float price() {
+        return 12.0f;
+    }
+
+    public String name() {
+        return "Pepsi";
+    }
+}
